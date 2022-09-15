5618124898dbf8a0fb8c36fcebd14df55fc6f86a
kiril <po229-snigirev@yandex.ru>
2022-09-15 15:11:03 +0500

    6.2

--- FedSnig6/FedSnig6/urls.py
+++ FedSnig6/FedSnig6/urls.py
@@ -19,5 +19,8 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.BlogListView.as_view(), name='home')
+    path('', views.BlogListView.as_view(), name='home'),
+    path('post/new/', views.BlogCreateView.as_view(), name = 'post_new'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name = 'post_detail'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name = 'post_edit'),
 ]
--- FedSnig6/blog/views.py
+++ FedSnig6/blog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Post
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    templates_name = 'post_edit.html'
